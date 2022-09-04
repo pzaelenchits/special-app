8b9622c61288ec8dadb109f9d606bca5a3e64644
Pavel Zaelenchits <p.zaelenchits@yandex.rugit config --global user.name Pavel>
2022-09-04 17:51:47 +0700

    some changes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Date());
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+        System.out.println(formatter.format(new Date()));
     }
 
 
